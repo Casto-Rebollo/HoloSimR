ac5221bd322017d2d6231dd5d6b0a97ead9e8b4a
Casto-Rebollo <56078440+Casto-Rebollo@users.noreply.github.com>
2025-05-23 08:18:23 +0000

    varMH updated

--- data/code.R
+++ data/code.R
@@ -1,8 +1,9 @@
 ##Setting simulation parameters
-gSP = GlobalSP$new(nPop = 100, nChr = 3, nQTLchr = 100, nyear = 100,
-                  nSire = 20, nDam = 500)
+gSP <- GlobalSP$new(nPop = 100, nChr = 3, nQTLchr = 100, nyear = 100,nSire = 20, nDam = 500)
 gSP$setTrait(meanP = 3.66, varP = 0.46, h2 = 0.16, m2 = 0.13)
-gSP$setSpecies (nSpEff = 25)
+gSP$setSpecies(nSpEff = 25, varMH = 0.03, meanMH = 0.47)
+
+gSP$model <- "H"
 gSP$dataM <- "data/example.txt"
 
 ##Founder population 
